2e750fbeed3a34ee7c064c8c6e4d95c3c1e1681a
Nchargui <123753543+Nchargui@users.noreply.github.com>
2025-05-21 19:09:32 -0400

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,18 +1,18 @@
-resource "aws_key_pair" "tp_final_2283110_key" {
-  key_name   = "tp_final_2283110_key"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
+#resource "aws_key_pair" "tp_final_2283110_key" {
+ # key_name   = "tp_final_2283110_key"
+  #public_key = tls_private_key.rsa.public_key_openssh
+#}
 
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+#resource "tls_private_key" "rsa" {
+ # algorithm = "RSA"
+  #rsa_bits  = 4096
 
-}
+#}
 
-resource "local_file" "cluster_keypair" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = "${path.module}/tp_final_2283110_key.pem"
-}
+#resource "local_file" "cluster_keypair" {
+ # content  = tls_private_key.rsa.private_key_pem
+  #filename = "${path.module}/tp_final_2283110_key.pem"
+#}
 
 
 ###### INSTANCE
@@ -26,7 +26,7 @@ resource "aws_instance" "tp_final_instance_2283110" {
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.security_group_2283110.id]
 
-  key_name = aws_key_pair.tp_final_2283110_key.key_name
+  key_name = "tp_final_2283110_key"
 
   tags = {
     Name = "tp-final-instance-2283110"
