b69c1fe8dc983f080324c7881361397040344161
Nchargui <123753543+Nchargui@users.noreply.github.com>
2025-05-20 23:11:11 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -160,7 +160,6 @@ resource "aws_security_group" "security_group_2283110" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 }
 
